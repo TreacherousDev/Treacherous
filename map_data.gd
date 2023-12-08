c098bfa1cd9a7e2f218d7876bb027729c4e224c1
TreacherousDev <treacherousdev@gmail.com>
2023-12-08 22:26:00 +0800

    update resource

--- map_data.gd
+++ map_data.gd
@@ -4,3 +4,6 @@ class_name MapData
 @export var cell_id: int = 1
 @export var position: Vector2i
 @export var tree_id: int = 1
+@export var map_size: int = 20
+enum expand_modes {MAX, MIN, RANDOM}
+@export var expand_mode := expand_modes.RANDOM
